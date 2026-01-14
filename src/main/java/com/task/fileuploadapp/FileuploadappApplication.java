e51029775dbc2b74de2bcd3aef4bbf7e4cd53029
tjgusdnjs789@gmail.com <tjgusdnjs789@gmail.com>
2026-01-14 09:37:22 +0900

    [Init] init spring boot 4.0.1 java 17

---
+++ src/main/java/com/task/fileuploadapp/FileuploadappApplication.java
@@ -0,0 +1,13 @@
+package com.task.fileuploadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/fileuploadapp/FileuploadappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.fileuploadapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
